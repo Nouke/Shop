dc7853486cd4552f749ccfd7c62bd0972a46153e
Nouke souare <nsouare2006@gmail.com>
2022-01-11 18:10:53 +0100

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,87 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from '@material-ui/core';
 
 const Container = styled.div`
 height: 60px;
-background-color: black;
+
 `
+//background-color: black;
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+`;
+
+const Center = styled.div`
+flex: 1;
+text-align: center;
+`;
+const Right = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+`;
+const Language = styled.div`
+font-size: 14px;
+cursor: pointer;
+`;
+const SearchContainer = styled.div`
+border: 0.5px solid lightgray;
+display: flex;
+align-items: center;
+margin-left: 25px;
+padding: 5px;
+`;
+
+const Input = styled.div`
+border: none;
+`;
+
+const Logo = styled.h1`
+font-weight: bold;
+`;
+
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left: 25px;
+`;
 
 const Navbar = () => {
     return (
         <Container>
-            navbar
+        <Wrapper>
+            <Left>
+                    <Language>En</Language>
+                    <SearchContainer>
+                        <Input/>
+                        <Search style={{color:"gray", fontSize:16}}/>
+                    </SearchContainer>
+            </Left>
+            <Center><Logo>Nouk</Logo></Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined/>
+
+                    </Badge>
+                    </MenuItem>
+            </Right>
+        </Wrapper>
         </Container>
-    )
-}
+    );
+};
 
 export default Navbar
---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import { Container } from '@material-ui/core'
+import React from 'react'
+
+const Slider = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Slider
